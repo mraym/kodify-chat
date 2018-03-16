e6301a3c887c3c99cb9ce74104ff9b3773c36cb3
privateapps <privateappsnow@gmail.com>
2018-03-16 22:41:54 +0100

    Started kodify chat at Friday, 16-Mar 10:30pm CET

---
+++ README.md
@@ -0,0 +1,24 @@
+# Kodify Chat
+
+## Setup
+
+1. Create package.json
+```
+{
+  "name": "kodify-chat",
+  "version": "0.0.1",
+  "description": "chat for kodify",
+  "dependencies": {}
+}
+```
+
+2. Install packages
+```
+npm install express --save
+npm install socket.io --save
+```
+
+## Start the server
+```
+node index.js
+```
---
+++ index.js
@@ -0,0 +1,19 @@
+var express = require("express");
+var sockio = require("socket.io");
+
+var app = express();
+
+// serve static files from the "public" directory
+app.use(express.static('public'));
+
+// start the app on port specified
+var portNum = 3000;
+var io = sockio.listen(app.listen(portNum), {log: true});
+console.log("NodeJS SocketIO Server started on port " + portNum);
+
+// listend for chat messages
+io.on('connection', function(socket){
+  socket.on('chat message', function(msg){
+    io.emit('chat message', msg);
+  });
+});
